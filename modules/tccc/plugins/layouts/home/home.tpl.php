75e42d645321d7de0872f0589bdc403caa0899b6
Erik Erskine <erik@brightec.co.uk>
2011-10-04 15:18:50 +0100

    HTML5 elements on panel panes

--- modules/tccc/plugins/layouts/home/home.tpl.php
+++ modules/tccc/plugins/layouts/home/home.tpl.php
@@ -7,7 +7,7 @@
  */
 ?>
 
-<section class="layout-home panel-display clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+<article class="layout-home panel-display clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 
   <div class="wide-top grid_12 alpha omega">
     <div class="border"></div>  
@@ -15,24 +15,24 @@
   </div>
   
   <div class="grid_4 alpha">
-    <div class="panel-column-1">
+    <section class="panel-column-1">
       <div class="button_1"></div>
       <?php print $content['column-1']; ?>
-    </div>
+    </section>
   </div>
   
   <div class="grid_4">
-    <div class="panel-column-2">
+    <section class="panel-column-2">
       <div class="button_2"></div>
       <?php print $content['column-2']; ?>
-    </div>
+    </section>
   </div>
   
   <div class="grid_4 omega">
-    <div class="panel-column-3">
+    <section class="panel-column-3">
       <div class="button_3"></div>
       <?php print $content['column-3']; ?>
-    </div>
+    </section>
   </div>
   
-</section>
+</article>
